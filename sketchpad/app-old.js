291529cef985af627e4bf327de93afe383b86332
Dalton Craven <20dalton00@gmail.com>
2017-06-26 21:15:24 -0400

    fix firebase error

--- sketchpad/app-old.js
+++ sketchpad/app-old.js
@@ -1,9 +1,10 @@
 var config = {
-    apiKey: "AIzaSyDzWbmDmEHjl3QI8rWhYmLeueJVslkISc0",
-    authDomain: "collaborative-sketch-d9e2d.firebaseapp.com",
-    databaseURL: "https://collaborative-sketch-d9e2d.firebaseio.com",
-    storageBucket: "collaborative-sketch-d9e2d.appspot.com",
-    messagingSenderId: "735562726709"
+  apiKey: "AIzaSyDzWbmDmEHjl3QI8rWhYmLeueJVslkISc0",
+  authDomain: "collaborative-sketch-d9e2d.firebaseapp.com",
+  databaseURL: "https://collaborative-sketch-d9e2d.firebaseio.com",
+  projectId: "collaborative-sketch-d9e2d",
+  storageBucket: "collaborative-sketch-d9e2d.appspot.com",
+  messagingSenderId: "735562726709"
 };
 firebase.initializeApp(config);
 
@@ -13,14 +14,14 @@ var toolType = 'dot';
 var connectLine;
 
 function setup() {
-    var canvas = createCanvas(500,500);
-    canvas.parent('sketch');
-    background(255);
-    fill(0);
-    pointsData.on("child_added", function (point) {points.push(point.val());});
-    pointsData.on("child_removed", function () {points = [];});
-    canvas.mousePressed(drawPoint);
-    canvas.mouseMoved(drawPointIfMousePressed);
+  var canvas = createCanvas(500,500);
+  canvas.parent('sketch');
+  background(255);
+  fill(0);
+  pointsData.on("child_added", function (point) {points.push(point.val());});
+  pointsData.on("child_removed", function () {points = [];});
+  canvas.mousePressed(drawPoint);
+  canvas.mouseMoved(drawPointIfMousePressed);
 }
 
 function mousePressed() {
@@ -50,12 +51,12 @@ function draw() {
 
 function drawPoint() {
   pointsData.push({
-      x: mouseX,
-      y: mouseY,
-      type: toolType,
-      color: $("#inputColor").val(),
-      width: $("#inputWidth").val(),
-      connected: connectLine
+    x: mouseX,
+    y: mouseY,
+    type: toolType,
+    color: $("#inputColor").val(),
+    width: $("#inputWidth").val(),
+    connected: connectLine
   });
 }
 
@@ -69,11 +70,11 @@ function saveDrawing() {
 
 $("#clearDrawing").on("click", clearDrawing);
 function clearDrawing() {
-    var result = confirm("Are you sure you want to delete? This can't be undone!");
-    if (result) {
-        pointsData.remove();
-        points = [];
-    }
+  var result = confirm("Are you sure you want to delete? This can't be undone!");
+  if (result) {
+    pointsData.remove();
+    points = [];
+  }
 }
 
 $("#test").on("click", test);
@@ -92,14 +93,14 @@ function changeColor() {
 function changeType() {
   /*
   if(document.getElementById('checkLine').checked) {
-    toolType = "line";
-  } else {
-    toolType = "dot";
-  }
-  */
-  if(document.getElementById('checkLine').checked) {
-    toolType = "line";
-  } else {
-    toolType = "dot";
-  }
+  toolType = "line";
+} else {
+toolType = "dot";
+}
+*/
+if(document.getElementById('checkLine').checked) {
+  toolType = "line";
+} else {
+  toolType = "dot";
+}
 }
